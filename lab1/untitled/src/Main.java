09f70694cba8908d9f58842bd61db882d6ecda81
YuriiYurchuk <urthyk1234@gmail.com>
2024-02-13 14:35:14 +0200

    lab1 corrected

--- lab1/untitled/src/Main.java
+++ lab1/untitled/src/Main.java
@@ -1,28 +1,43 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {
+    private double x = 1.426;
+    private double y = -1.220;
+    private double z = 3.5;
+    private final Scanner scanner;
 
     public static void main(String[] args) {
         Main program = new Main();
         program.run();
     }
 
-    private void run() {
-        double x = 1.426;
-        double y = -1.220;
-        double z = 3.5;
-        double resultA = calculateA(x, y);
-        double resultB = calculateB(z);
-        print(resultA, resultB);
-        printDateTime();
+    public Main() {
+        scanner = new Scanner(System.in);
     }
 
-    private double calculateA(double x, double y) {
+    private void run() {
+        print(calculateA(), calculateB());
+        printDateTime();
+        System.out.println("Змінити значення x, y, та z? (1 - Так, 0 - Ні): ");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            System.out.print("x = ");
+            x = scanner.nextDouble();
+            System.out.print("y = ");
+            y = scanner.nextDouble();
+            System.out.print("z = ");
+            z = scanner.nextDouble();
+            print(calculateA(), calculateB());
+        }
+    }
+
+    private double calculateA() {
         return (2 * Math.cos(x - Math.PI / 6)) / (0.5 + Math.pow(Math.sin(y), 2));
     }
 
-    private double calculateB(double z) {
+    private double calculateB() {
         return 1 + z * z / (3 + z * z / 5);
     }
 
