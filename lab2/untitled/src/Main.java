de9631acb055c76155e74371ed74fad501a1e5e3
YuriiYurchuk <urthyk1234@gmail.com>
2024-02-27 20:29:51 +0200

    corrected

--- lab2/untitled/src/Main.java
+++ lab2/untitled/src/Main.java
@@ -118,7 +118,6 @@ public class Main {
     private static LibraryMember[] members = new LibraryMember[5];
 
     public static void main(String[] args) {
-        // Test data
         ArrayList<String> books1 = new ArrayList<>();
         books1.add("Book1");
         books1.add("Book2");
@@ -129,19 +128,14 @@ public class Main {
         books3.add("Book5");
         ArrayList<String> books4 = new ArrayList<>();
         books4.add("Book6");
-
-        // Creating objects
         members[0] = new LibraryMember("1", "Tymoshchenko", "Ivan", "Ivanovich", "+380123456789", "St. Main 124", books1, LocalDate.of(2022,12,1),true);
         members[1] = new LibraryMember("2", "Dyadchenko", "Ivanka", "Ivanivna", "+380987654321", "St. Volitsa 43", books2, LocalDate.of(2024,2,27), false);
         members[2] = new LibraryMember("3", "Gordienko", "Dmytro", "Dmytrovych", "+380456789123", "St. Independence 10", books3, LocalDate.of(2024,1,24), false);
         members[3] = new LibraryMember("4", "Belous", "Nina", "Ihorivna", "+380789123456", "St. Mountain 58", books4, LocalDate.of(2021,1,1), false);
         members[4] = new LibraryMember("5", "Popov", "Mykhailo", "Mykhailovych", "+380321654987", "St. Lisova 12", new ArrayList<>(), LocalDate.of(2024,2,12), true);
-
-        // Output test data
         for (LibraryMember member : members) {
             System.out.println(member);
         }
-
         // Example of using filtering methods
         filterByBorrowedBooks("Book1");
         filterByOverdueIncidents(true);
@@ -160,13 +154,9 @@ public class Main {
     public static void filterByLastVisitDate() {
         LocalDate currentDate = LocalDate.now();
         System.out.println("Members who haven't visited for more than a year:");
-
-        // Отримання поточного року
         int currentYear = currentDate.getYear();
-
-        // Порівняння дати останнього відвідування з поточним роком
         for (LibraryMember member : members) {
-            if (member != null) { // Перевірка на null перед викликом методу
+            if (member != null) {
                 int lastVisitYear = member.getLastVisitDate().getYear();
                 if (currentYear - lastVisitYear > 1) {
                     System.out.println(member);
